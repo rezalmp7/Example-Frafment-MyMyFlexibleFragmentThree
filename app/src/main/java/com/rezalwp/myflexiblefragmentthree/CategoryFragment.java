bfba08eb8b54620f731be3e561402fd8939bc2c9
rezalmp7 <rezalwahyu10@gmail.com>
2020-04-27 10:39:23 +0700

    rezal

---
+++ app/src/main/java/com/rezalwp/myflexiblefragmentthree/CategoryFragment.java
@@ -0,0 +1,61 @@
+package com.rezalwp.myflexiblefragmentthree;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CategoryFragment extends Fragment implements View.OnClickListener {
+
+    public CategoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnDetailCategory = view.findViewById(R.id.btn_detail_category);
+        btnDetailCategory.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_detail_category) {
+            DetailCategoryFragment mDetailCategoryFragment = new DetailCategoryFragment();
+
+            Bundle mBundle = new Bundle();
+
+            mBundle.putString(DetailCategoryFragment.EXTRA_NAME, "Lifestyle");
+            String description = "Kategory ini akan berisi produk-produk Lifestyle";
+
+            mDetailCategoryFragment.setArguments(mBundle);
+            mDetailCategoryFragment.setDescription(description);
+
+            FragmentManager mFragmentManager = getFragmentManager();
+            if (mFragmentManager != null) {
+                mFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_container, mDetailCategoryFragment, DetailCategoryFragment.class.getSimpleName())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        }
+    }
+}
